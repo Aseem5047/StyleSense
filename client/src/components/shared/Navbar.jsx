74570a96a1c26f7cd462c3763142d264a242ba3f
Aseem5047 <aseemgupta2002@gmail.com>
2024-04-02 15:57:56 +0530

    navbar logout logic updated

--- client/src/components/shared/Navbar.jsx
+++ client/src/components/shared/Navbar.jsx
@@ -34,6 +34,8 @@ const Navbar = () => {
 			// Remove token from localStorage and cookies
 			// Remove token cookie
 			removeCookie("token", { path: "/" });
+			// Set the random token as a cookie
+			setCookie("token", "Unauthorized Access", { path: "/" });
 			localStorage.clear();
 			// Dispatch action to clear user
 			dispatch(clearUser());
